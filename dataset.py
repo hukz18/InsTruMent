b0b8687ae0e81a11752c3cecdd33e8cb3cce7fbe
hukz <hukz18@mails.tsinghua.edu.cn>
2020-12-27 11:38:15 +0800

    add prediction function in supervised.py, add evaluation of supervised methods, auto-formatted unsuperivesd.py

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ import glob
 def get_data(label, number: int, true_p=0.3, is_train=True):
     # 接受所需的标签，返回对应的正类及负类文件名
     # 加一个标签，决定从训练集还是测试集读取
-    dataset_path = './IRMAS-TrainingData'
+    dataset_path = './dataset/IRMAS-TrainingData'
     all_pos_data = glob.glob(
         f'{dataset_path}/{label}/*.wav')
     pos_data_file = sample(all_pos_data, int(number * true_p))
@@ -17,7 +17,7 @@ def get_data(label, number: int, true_p=0.3, is_train=True):
     all_neg_file = glob.glob(
         f'{dataset_path}/[!{label[0]}][!{label[1]}][!{label[2]}]/*.wav')
 
-    neg_data_file = sample(all_neg_file, int(number * (1-true_p)))
+    neg_data_file = sample(all_neg_file, int(number * (1 - true_p)))
     # print(f'getting neg data : {neg_data_file}')
 
     all_file = pos_data_file + neg_data_file
@@ -34,11 +34,12 @@ def get_data(label, number: int, true_p=0.3, is_train=True):
     print(f'waves.shape = {waves.shape}')
     samples = np.array(samples)
     true_list = [True] * int(number * true_p) + \
-        [False] * int(number * (1-true_p))
+                [False] * int(number * (1 - true_p))
     true_list = np.array(true_list)
 
     p = np.random.permutation(len(true_list))
     return waves[p], samples[p], true_list[p]
+
 
 def get_data_list(label_list, number: int, true_p=0.3, is_train=True):
     # 接受所需的标签列表，返回对应的正类及负类文件名,true_p现在表示全部正类占比了
@@ -63,7 +64,7 @@ def get_data_list(label_list, number: int, true_p=0.3, is_train=True):
         neg_file = glob.glob(f'{dataset_path}/[!{label[0]}][!{label[1]}][!{label[2]}]/*.wav')
         all_neg_file += neg_file
 
-    neg_data_file = sample(all_neg_file, int(number * (1-true_p)))
+    neg_data_file = sample(all_neg_file, int(number * (1 - true_p)))
     # print(f'getting neg data : {neg_data_file}')
 
     all_file = pos_data_file + neg_data_file
@@ -80,13 +81,14 @@ def get_data_list(label_list, number: int, true_p=0.3, is_train=True):
     print(f'waves.shape = {waves.shape}')
     samples = np.array(samples)
     true_list = [True] * int(number * true_p) + \
-        [False] * int(number * (1-true_p))
+                [False] * int(number * (1 - true_p))
     true_list = np.array(true_list)
 
     p = np.random.permutation(len(true_list))
     return waves[p], samples[p], true_list[p]
 
-def get_data_list_weight(label_list, number: int, weight, is_train=True):
+
+def get_data_list_weighted(label_list, number: int, weight, is_train=True):
     # 接受所需的标签列表，返回对应的正类及负类文件名,true_p现在没了，用weight表示比例
     # 加一个标签，决定从训练集还是测试集读取
     dataset_path = './IRMAS-TrainingData'
@@ -95,7 +97,7 @@ def get_data_list_weight(label_list, number: int, weight, is_train=True):
     true_list = []
 
     pos_data_file = []
-    for i in range(0,11):
+    for i in range(0, 11):
         label = labels[i]
         if label in label_list:
             pos_file = glob.glob(f'{dataset_path}/{label}/*.wav')
@@ -103,7 +105,7 @@ def get_data_list_weight(label_list, number: int, weight, is_train=True):
             true_list += [True] * int(number * weight[i])
 
     neg_data_file = []
-    for i in range(0,11):
+    for i in range(0, 11):
         label = labels[i]
         if label in label_list:
             continue
@@ -130,27 +132,29 @@ def get_data_list_weight(label_list, number: int, weight, is_train=True):
     p = np.random.permutation(len(true_list))
     return waves[p], samples[p], true_list[p]
 
-def get_label_test():
-    #读取测试集wave和sample，还有标签，返回迭代器
 
-    dataset_path = './IRMAS-TestingData-Part1/Part1'
+def get_test_data(num=None):
+    # 读取测试集wave和sample，还有标签，返回迭代器
+
+    dataset_path = './dataset/IRMAS-TestingData-Part1/Part1'
     all_data = glob.glob(f'{dataset_path}/*.wav')
-    #打乱
+    # 打乱
     shuffle(all_data)
-
+    if num is not None:
+        all_data = all_data[:num]
     for file in all_data:
         w, s = librosa.load(file)
         # print(w.shape)
         w = np.array(w)
         s = np.array(s)
 
-        label_path = file[0:-3]+'txt'
-        txtfile = open(label_path,'r')
+        label_path = file[0:-3] + 'txt'
+        txtfile = open(label_path, 'r')
         true_class = [x.strip() for x in txtfile]
         txtfile.close()
         yield w, s, true_class
 
 
 if __name__ == '__main__':
-    for w,s,true_class in get_label_test():
+    for w, s, true_class in get_test_data():
         print(true_class)
--- unsuperivesd.py
+++ unsuperivesd.py
@@ -1,4 +1,3 @@
-# 江姐自由发挥吧.. 接受features，返回聚类结果,
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -9,67 +8,90 @@ from sklearn.mixture import GaussianMixture
 from sklearn.cluster import AgglomerativeClustering
 from sklearn import manifold
 
-def pca_plot(X):# 画PCA的拐点图，差不多选前3个或者前2的变量没啥问题
+
+def pca_plot(X):  # 画PCA的拐点图，差不多选前3个或者前2的变量没啥问题
     PCA_model = PCA(n_components=10)
     PCA_model.fit(X)
     PCA_var = PCA_model.explained_variance_
     plt.plot(np.arange(0, 10), PCA_var)
+    plt.show()
 
-def pca_decomposition(X,n,labels,n_count=100,picshow=0): #撒豆子警告
+
+def pca_decomposition(X, n, labels, n_count=100, picshow=0):  # 撒豆子警告
     warnings.filterwarnings('ignore')
     sns.set()
     PCA_model = PCA(n_components=n)
     PCA_model.fit(X)
-    X_new=PCA_model.fit_transform(X)
-    fig = plt.figure(figsize=(20,15))
-    if((n==3)&picshow):
+    X_new = PCA_model.fit_transform(X)
+    fig = plt.figure(figsize=(20, 15))
+    if n == 3 and picshow:
         colors = sns.color_palette("Spectral", len(labels))
-        #colors=["#9E0142","#D53E4F","#F46D43","#FDAE61", "#FEE08B","#FFFFBF","#E6F598","#ABDDA4","#66C2A5","#3288BD","#5E4FA2"]
+        # colors=["#9E0142","#D53E4F","#F46D43","#FDAE61", "#FEE08B","#FFFFBF","#E6F598","#ABDDA4","#66C2A5","#3288BD","#5E4FA2"]
         ax = fig.add_subplot(111, projection='3d')
-        for i in range(0,len(labels)):
-            x =X_new[i*n_count:(i+1)*n_count,0]
-            y =X_new[i*n_count:(i+1)*n_count,1]
-            z =X_new[i*n_count:(i+1)*n_count,2]
-            ax.scatter(x, y, z, c=colors[i], marker='o',label=labels[i],s=10)
+        for i in range(0, len(labels)):
+            x = X_new[i * n_count:(i + 1) * n_count, 0]
+            y = X_new[i * n_count:(i + 1) * n_count, 1]
+            z = X_new[i * n_count:(i + 1) * n_count, 2]
+            ax.scatter(x, y, z, c=colors[i], marker='o', label=labels[i], s=10)
         plt.legend()
         plt.show()
 
-    if((n==2)&picshow):
+    if n == 2 & picshow:
         colors = sns.color_palette("Spectral", len(labels))
-        for i in range(0,len(labels)):
-            x =X_new[i*n_count:(i+1)*n_count,0]
-            y =X_new[i*n_count:(i+1)*n_count,1]
-            plt.scatter(x, y, c=colors[i], marker='o',label=labels[i],s=10)
+        for i in range(0, len(labels)):
+            x = X_new[i * n_count:(i + 1) * n_count, 0]
+            y = X_new[i * n_count:(i + 1) * n_count, 1]
+            plt.scatter(x, y, c=colors[i], marker='o', label=labels[i], s=10)
         plt.legend()
         plt.show()
 
-    return(X_new)
+    return X_new
 
-def outcome_print(Y_predict,labels,n_count=100): #聚类结果可视化
-    count_martix=np.zeros([len(labels),max(Y_predict)+1],dtype=int)
-    for i in range(0,len(labels)):
-        label_count = np.bincount(Y_predict[i*n_count:(i+1)*n_count])
-        count_martix[i,0:len(label_count)]=label_count
-        print(labels[i],"| max class =",np.argmax(label_count),"| count =",label_count[np.argmax(label_count)])
-        #print(labels[i],np.argmax(label_count),label_count)
-    ax1 = sns.heatmap(count_martix,cmap='rainbow',annot=True)
+
+def outcome_print(Y_predict, labels, n_count=100):  # 聚类结果可视化
+    count_martix = np.zeros([len(labels), max(Y_predict) + 1], dtype=int)
+    for i in range(0, len(labels)):
+        label_count = np.bincount(Y_predict[i * n_count:(i + 1) * n_count])
+        count_martix[i, 0:len(label_count)] = label_count
+        print(labels[i], "| max class =", np.argmax(label_count), "| count =", label_count[np.argmax(label_count)])
+        # print(labels[i],np.argmax(label_count),label_count)
+    ax1 = sns.heatmap(count_martix, cmap='rainbow', annot=True)
     ax1.set_yticklabels(labels)
-    return(count_martix)
+    return count_martix
 
-def gmm_predict(X,n_centers=11,type="diag"): #高斯混合模型
-   gmm =  GaussianMixture(n_components=11,covariance_type=type).fit(X)
-   Y_predict = GaussianMixture.predict(gmm,X)#predict_proba
-   return(Y_predict)
 
-def hierarchical_predict(X,n_centers=30,linkage="ward"): #层次聚类
+def gmm_predict(X, n_centers=11, type="diag"):  # 高斯混合模型
+    """
+    :param n_centers: 聚类数目
+    :param type: 表现最好的方法
+    :return:
+    """
+    gmm = GaussianMixture(n_components=11, covariance_type=type).fit(X)
+    Y_predict = GaussianMixture.predict(gmm, X)  # predict_proba
+    return Y_predict
+
+
+def hierarchical_predict(X, n_centers=30, linkage="ward"):  # 层次聚类
     ac = AgglomerativeClustering(n_clusters=n_centers, affinity='euclidean', linkage=linkage)
     # {"ward", "complete", "average", "single"}
     ac.fit(X)
     Y_predict = ac.fit_predict(X)
-    return(Y_predict)
+    return Y_predict
+
 
 def t_SNE2(X):
     print("Computing t-SNE embedding")
     tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
     X_tsne = tsne.fit_transform(X)
+    return X_tsne
+
+
+def get_tsne():
+    """
+    获取数据点的tsne聚类
+    :return: 聚类后各点的坐标
+    TODO:带标签聚类并可视化
+    """
+    X = np.loadtxt("read_mfcc.txt", dtype=int, delimiter=" ")
+    X_tsne = unsuperivesd.t_SNE2(X)
     return X_tsne
