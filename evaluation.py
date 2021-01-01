0184f20e7a66242efe7aebe0437ad6e150a0b2f1
BigBroBob <baozihz@163.com>
2021-01-01 20:14:03 +0800

    co_est_mat

--- evaluation.py
+++ evaluation.py
@@ -25,7 +25,18 @@ def confusion_mat(true,est):
 
     return confusion
 
+def co_est_mat(true,est):
+    co_est = np.mat(np.zeros((np.size(true,1),np.size(true,1))))
+    total = np.zeros((np.size(true,1)))
+    for i in range(np.size(co_est,1)):
+        for j in range(np.size(true,0)):
+            if true[j,i] == 1:
+                co_est[i] += est[j]
+                total[i] += 1
+    co_est = np.true_divide(co_est,np.transpose(np.mat(total)))
+    return co_est
+
 if __name__ == '__main__':
     a = np.mat([[1,0,0],[1,0,0],[0,1,0],[0,0,1]])
     b = np.mat([[0,1,0],[0,1,1],[1,0,0],[0,1,0]])
-    c = confusion_mat(a,b)
+    c = co_est_mat(a,b)
